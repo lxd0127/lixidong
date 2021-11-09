0f63da5fa895f78c65af6bb6b67a5903f1f040a6
lxd0127 <1227434234@qq.com>
2021-11-09 22:36:05 +0800

    第三次

--- 第三次lv0/第三次lv0使用构建函数.js
+++ 第三次lv0/第三次lv0使用构建函数.js
@@ -1,8 +1,6 @@
-class handsomeBoy {
-    constructor() {
-        this.名字 = 'liXiDong';
-        this.年龄 = 18;
-    }
+function headsomeBoy(){
+    this.名字 = 'liXiDong';
+    this.年龄 = 18;
 }
-let B = new handsomeBoy();
+let B = new headsomeBoy();
 console.log (B); 
